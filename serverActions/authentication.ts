84ac3efd04bab7429a2c9feeee35524121fdc98e
MaSch <Mathias.scheltjens@gmail.com>
2023-10-18 15:49:46 +0200

    refactor of old project, initial commit

--- serverActions/authentication.ts
+++ serverActions/authentication.ts
@@ -1,24 +1,24 @@
-'use server';
+// 'use server';
 
-import { hash } from 'bcrypt';
+// import { hash } from 'bcrypt';
 
-import { RegisterFormSchema } from '@/lib/schemas/authenticationFormSchemas';
+// import { RegisterFormSchema } from '@/lib/schemas/authenticationFormSchemas';
 
-export const registerUser = async (formData: unknown) => {
-  const result = RegisterFormSchema.safeParse(formData);
-  if (!result.success) {
-    let message = '';
-    result.error?.issues.forEach((error) => {
-      message = message += `${error.message}\n`;
-    });
-    return { error: 'Invalid data' };
-  }
-  const newUser = {
-    name: `${result.data.firstName} ${result.data.lastName}`,
-    email: result.data.email,
-    password: await hash(result.data.password, 10),
-  };
-  // const user = await prismadb.user.create({ data: newUser });
-  const user = await db.insert.users.one(newUser);
-  return { success: true, user };
-};
+// export const registerUser = async (formData: unknown) => {
+//   const result = RegisterFormSchema.safeParse(formData);
+//   if (!result.success) {
+//     let message = '';
+//     result.error?.issues.forEach((error) => {
+//       message = message += `${error.message}\n`;
+//     });
+//     return { error: 'Invalid data' };
+//   }
+//   const newUser = {
+//     name: `${result.data.firstName} ${result.data.lastName}`,
+//     email: result.data.email,
+//     password: await hash(result.data.password, 10),
+//   };
+//   // const user = await prismadb.user.create({ data: newUser });
+
+//   return { success: true, user };
+// };
--- serverActions/createLaboratory.ts
+++ serverActions/createLaboratory.ts
@@ -1,20 +1,20 @@
-'use server';
+// 'use server';
 
-import { getAuthServerSession } from '@/utils/serverSession';
+// import { getAuthServerSession } from '@/utils/serverSession';
 
-export const createLaboratory = async (name: string) => {
-  // try {
-  //   const alreadyExists = await prismadb.laboratory.findFirst({
-  //     where: { name },
-  //   });
-  //   if (alreadyExists) {
-  //     return { error: 'Labo already exists' };
-  //   }
-  //   const response = await prismadb.laboratory.create({
-  //     data: { name, user: { connect: { id: (await getAuthServerSession())?.user.id } } },
-  //   });
-  //   return { success: 'Labo created', response };
-  // } catch (error) {
-  //   return { error: 'Error creating labo' };
-  // }
-};
+// export const createLaboratory = async (name: string) => {
+//   try {
+//     const alreadyExists = await prismadb.laboratory.findFirst({
+//       where: { name },
+//     });
+//     if (alreadyExists) {
+//       return { error: 'Labo already exists' };
+//     }
+//     const response = await prismadb.laboratory.create({
+//       data: { name, user: { connect: { id: (await getAuthServerSession())?.user.id } } },
+//     });
+//     return { success: 'Labo created', response };
+//   } catch (error) {
+//     return { error: 'Error creating labo' };
+//   }
+// };
